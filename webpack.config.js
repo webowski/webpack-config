30cb8340aad7c3fba534757d4c8af6805c3b722b
Aleksandr Osokin <sanvovanych@gmail.com>
2021-02-03 12:47:15 +0300

    added the styles rule, hotreloading, browserslist

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,33 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 let mode = process.env.NODE_ENV || 'development'
 let target = mode === 'development' ? 'web' : 'browserslist'
+
+const postcssOptions = {
+	postcssOptions: {
+		plugins: ['postcss-preset-env']
+	}
+}
 
 module.exports = {
 	mode: mode,
 
 	module: {
 		rules: [
+
+			// Styles
+			{
+				test: /\.(s[ac]|c)ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: postcssOptions
+					},
+					'sass-loader',
+				]
+			},
 
 			// Scripts
 			{
@@ -23,8 +44,14 @@ module.exports = {
 		]
 	},
 
+	plugins: [
+		new MiniCssExtractPlugin()
+	],
+
+	target: target,
 	devtool: 'source-map',
 	devServer: {
-		contentBase: './dist'
+		contentBase: './dist',
+		hot: true,
 	}
 }
