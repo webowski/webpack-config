6561641ad93a1fc698ac7269dc1c99dc2bc682b4
Aleksandr Osokin <sanvovanych@gmail.com>
2021-02-04 12:35:29 +0300

    enabled babel cache

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,8 @@ module.exports = {
 						presets: [
 							'@babel/preset-env',
 							['@babel/preset-react', { runtime: 'automatic' }],
-						]
+						],
+						cacheDirectory: true,
 					}
 				}
 			}
