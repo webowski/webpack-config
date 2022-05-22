a331539226566f4c4e13b8715e68e7395c4747bb
Aleksandr Osokin <sanvovanych@gmail.com>
2022-05-22 08:33:56 +0300

    Browserslist, styles webpack packages, styles config, SCSS

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,14 +1,51 @@
 import { resolve } from 'path'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
 let mode = 'development'
+let target = 'web'
 
-if (process.env.NODE_ENV === 'production') mode = 'production'
+if (process.env.NODE_ENV === 'production') {
+	mode = 'production'
+	target = 'browserslist'
+}
 
 export default {
 	mode: mode,
+	target: target,
+
+	entry: {
+		// styles: {
+		// 	import: resolve('./src/styles/index.scss'),
+		// 	filename: './styles/[name].min.js'
+		// },
+		bundle: {
+			import: resolve('./src/scripts/index.js'),
+			filename: './scripts/[name].min.js'
+		}
+	},
 
 	module: {
 		rules: [
+
+			// Styles
+			{
+				test: /\.(scss|css)$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [
+									['postcss-preset-env']
+								]
+							}
+						}
+					},
+					'sass-loader'
+				]
+			},
 
 			// Scripts
 			{
@@ -28,10 +65,31 @@ export default {
 		]
 	},
 
+	plugins: [
+
+		new MiniCssExtractPlugin({
+			filename: 'styles/styles.min.css',
+		}),
+
+	],
+
 	devtool: 'source-map',
 	devServer: {
+		hot: true,
 		static: {
 			directory: resolve('dist')
-		}
-	}
+		},
+		// devMiddleware: {
+		// 	writeToDisk : true
+		// }
+	},
+
+	// optimization: {
+	// 	// minimize: false,
+	// 	// runtimeChunk: 'single',
+	// 	runtimeChunk: {
+	// 		name: 'runtime',
+	// 	},
+	// },
+
 }
